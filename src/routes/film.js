e14da565c8f8e0c4e78a8b5d5699dad05bec8356
Alpasc <alinepascal@gmail.com>
2020-07-25 12:31:11 +0200

    changement route get film

--- src/routes/film.js
+++ src/routes/film.js
@@ -8,7 +8,7 @@ router.post('/', (req, res) => {
   connection.query('INSERT INTO film SET ?', formData, (err, results) => {
     // console.log(err);
     if (err) {
-      // console.log(err);
+      console.log(err);
       res.status(500).send('Erreur dans l\'insertion d\'un film');
     } else {
       res.sendStatus(200);
@@ -17,7 +17,7 @@ router.post('/', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  connection.query('SELECT * FROM film', (error, results) => {
+  connection.query('SELECT film.id AS id, film.nom AS nom, realisateur, acteurs, genre.nom AS genre, annee_sortie, pays, trailer, affiche, synopsis, commentaire FROM film JOIN genre ON genre.id=film.genre_id ORDER BY nom ASC', (error, results) => {
     res.status(200).json({
       status: 'success',
       results,
@@ -27,7 +27,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  const query = 'SELECT * FROM film WHERE id = ?';
+  const query = 'SELECT * FROM film FULL JOIN genre ON film.genre_id = genre.id WHERE film.id = ?';
   connection.query(query, id, (error, results) => {
     try {
       if (results.length === 0) {
@@ -90,7 +90,6 @@ router.delete('/:id', (req, res) => {
       } else {
         res.status(201).json({
           status: 'success',
-          filmDeleted: id,
         });
       }
     } catch (err) {
